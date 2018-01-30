1738c624274ac9fc79aecbcde2522d0fd8f4aa7a
Oleksandr Krysiuk <oleksandr.krysiuk@gmail.com>
2018-01-30 16:45:34 +0200

    Part_2

--- js/app.js
+++ js/app.js
@@ -13,6 +13,12 @@ loadEventListeners();
 function loadEventListeners(){
   //Add task event
   form.addEventListener('submit', addTask);
+  //Remove task event
+  taskList.addEventListener('click', removeTask);
+  //Clear task event
+  clearBtn.addEventListener('click', clearTasks);
+  //Filter tasks events
+  filter.addEventListener('keyup', filterTasks);
 }
 
 //Add task
@@ -40,4 +46,39 @@ function addTask(e){
   //Clear input
   taskInput.value = '';
   e.preventDefault();
+}
+
+//Remove Task
+
+function removeTask(e){
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Are you sure ?')) {
+    e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+
+//Clear Tasks
+
+function clearTasks() {
+  //taskList.innerHTML = '';
+  //Faster
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+//Filter tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach (function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
